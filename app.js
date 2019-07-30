41888adde3e403d6aed576be416b986f8f1ef367
afdhal-PC\afdhal <awakenblueheart@gmail.com>
2019-07-30 22:45:07 +0800

    Update app.js
    change the port from 3000 to 80, for internet public

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ io.on('connection', socket => {
     })
 })
 
-http.listen(3000, function() {
-   console.log('listening on *:3000');
+http.listen(80, function() {
+   console.log('listening on *:80');
 });
 
