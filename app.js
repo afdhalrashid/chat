0ed50c07f1974727e75948a69bf607dfbcf4b4ac
afdhal-PC\afdhal <awakenblueheart@gmail.com>
2019-07-30 22:52:19 +0800

    Update app.js
    port lagi

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ io.on('connection', socket => {
         delete users[socket.id]
     })
 })
-
-http.listen(80, function() {
-   console.log('listening on *:80');
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, function() {
+   console.log('listening on ${ PORT }');
 });
 
